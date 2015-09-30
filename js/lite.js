46cbcfcb1ff8e3720d5c4107efef6dc42a0e2b60
muxistudio <muxistudio@qq.com>
2015-09-30 15:30:49 +0800

    add lite-dom

---
+++ js/lite.js
@@ -0,0 +1,28 @@
+/***
+Litejs_dom v0.0.1
+***/
+define([], function () {
+    var _hasClass = function (elem, className) {
+        return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
+    }
+
+    var addClass = function (elem, className) {
+        if (!_hasClass(elem, className)) {
+            elem.className += ' ' + className;
+        }
+    }
+
+    var removeClass = function (elem, className) {
+        var newClass = ' ' + elem.className.replace(/[\t\r\n]/g, ' ') + ' ';
+        if (_hasClass(elem, className)) {
+            while (newClass.indexOf(' ' + className + ' ') >= 0) {
+                newClass = newClass.replace(' ' + className + ' ', ' ');
+            }
+            elem.className = newClass.replace(/^\s+|\s+$/g, '');
+        }
+    }
+    return {
+        addClass : addClass,
+        removeClass : removeClass
+    }
+})
---
+++ js/main.js
@@ -0,0 +1,5 @@
+requirejs(["lite"], function(l) {
+    var ele = document.getElementById('ele');
+    l.addClass(ele,'test3');
+    //l.addClass(ele,'test4');
+});
