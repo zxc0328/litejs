666c7213e8bd3c310bda38fc5dfd9b294d91eccf
zxc0328 <120797887@qq.com>
2016-06-22 19:15:17 +0800

    update karma

--- src/core.js
+++ src/core.js
@@ -1,118 +1,118 @@
 /* @flow */
 
 
-  var plainObj = {}
-  var hasOwn = plainObj.hasOwnProperty
+var plainObj = {}
+var hasOwn = plainObj.hasOwnProperty
 
-  var lite = function(selector, context) {
-    return new lite.prototype.init(selector, context)
-  }
-  
-  lite.prototype = {
+var lite = function(selector, context) {
+  return new lite.prototype.init(selector, context)
+}
 
-    constructor: lite,
+lite.prototype = {
 
-    // Start with an empty selector
-    selector: "",
+  constructor: lite,
 
-    // The default length of a lite object is 0
-    length: 0,
+  // Start with an empty selector
+  selector: "",
 
-    // Because window.window === window
-    isWindow: function(obj) {
-      return obj != null && obj === obj.window
-    },
+  // The default length of a lite object is 0
+  length: 0,
+}
 
-    isFunction: function(val) {
-      return plainObj.toString.call(val) === "[object Function]"
-    },
+/**
+ * Merge the contents of two or more objects together into the first object.
+ * lite.extend( target [, object1 ] [, objectN ] )
+ * lite.extend( [deep ], target, object1 [, objectN ] )
+ */
+lite.extend = lite.prototype.extend = function() {
+  var deep = false
+  var key
+  var source
+  var clone 
+  var src
+  var copy
+  var i = 1
+  var copyIsArray 
+  var target = arguments[0] || {}
 
-    // Check to see if an object is a plain object (created using "{}" or "new Object").
-    isPlainObject: function(obj) {
-      var key
-
-      if (typeof obj == "object" || obj.nodeType || obj.isWindow(obj)) {
-        return false
-      }
-
-      if (obj.constructor &&
-        !hasOwn.call(obj, "constructor") &&
-        !hasOwn.call(obj.constructor.prototype || {}, "isPrototypeOf")) {
-        return false
-      }
-
-      for (key in obj) {}
-
-      return key === undefined || hasOwn.call(obj, key)
-    },
-
-    merge: function() {
-
-    },
-
-    isArray: Array.isArray
+  if (typeof target === "boolean") {
+    deep = target
+    target = arguments[i] || {}
+    i++
   }
 
-  /**
-   * Merge the contents of two or more objects together into the first object.
-   * lite.extend( target [, object1 ] [, objectN ] )
-   * lite.extend( [deep ], target, object1 [, objectN ] )
-   */
-  lite.extend = lite.prototype.extend = function() {
-    var deep
-    var key
-    var source
-    var src
-    var copy
-    var i = 1
-    var target = arguments[0] || {}
+  if (typeof target !== "object" && !lite.isFunction(target)) {
+    target = {};
+  }
 
-    if (typeof target === "boolean") {
-      deep = target
-      target = arguments[i] || {}
-      i++
-    }
+  if (i === arguments.length) {
+    target = this
+    i--
+  }
 
-    if (typeof target !== "object" && !lite.isFunction(target)) {
-      target = {};
-    }
+  for (; i < arguments.length; i++) {
+    if ((source = arguments[i]) != null) {
+      for (key in source) {
+        src = target[key]
+        copy = source[key]
 
-    if (i === arguments.length) {
-      target = this
-      i--
-    }
+        // prevent circular reference
+        if (target === copy) {
+          continue
+        }
+        
+        if (deep && copy && (lite.isPlainObject(copy) ||
+            (copyIsArray = lite.isArray(copy)))) {
 
-    for (; i < arguments.length; i++) {
-      if ((source = arguments[i]) != null) {
-        for (key in source) {
-          src = target[key]
-          copy = source[key]
-
-          // prevent circular reference
-          if (target === copy) {
-            continue
+          if (copyIsArray) {
+            copyIsArray = false
+            clone = src && lite.isArray(src) ? src : []
+          } else {
+            clone = src && lite.isPlainObject(src) ? src : {}
           }
 
-          if (deep && copy && (lite.isPlainObject(copy) ||
-              (copyIsArray = lite.isArray(copy)))) {
-
-            if (copyIsArray) {  
-              copyIsArray = false
-              clone = src && lite.isArray(src) ? src : []
-            } else {
-              clone = src && lite.isPlainObject(src) ? src : {}
-            }
-
-            target[key] = lite.extend(deep, clone, copy)
-          }else if (copy !== undefined) {
-            target[key] = copy
-          }
+          target[key] = lite.extend(deep, clone, copy)
+        } else if (copy !== undefined) {
+          target[key] = copy
         }
       }
     }
-
-    return target
   }
+
+  return target
+}
+
+lite.extend({
+  // Because window.window === window
+  isWindow: function(obj) {
+    return obj != null && obj === obj.window
+  },
+
+  isFunction: function(val) {
+    return plainObj.toString.call(val) === "[object Function]"
+  },
+
+  // Check to see if an object is a plain object (created using "{}" or "new Object").
+  isPlainObject: function(obj) {
+    var key
+
+    if (typeof obj !== "object" || obj.nodeType || lite.isWindow(obj)) {
+      return false
+    }
+
+    if (obj.constructor &&
+      !hasOwn.call(obj, "constructor") &&
+      !hasOwn.call(obj.constructor.prototype || {}, "isPrototypeOf")) {
+      return false
+    }
+
+    for (key in obj) {}
+
+    return key === undefined || hasOwn.call(obj, key)
+  },
+
+  isArray: Array.isArray
+})
 
 // lite.prototype.init.prototype = lite.prototype
 
